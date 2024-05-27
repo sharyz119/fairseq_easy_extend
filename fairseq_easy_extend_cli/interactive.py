771ff1a268491181d7c70a98b7082eb09fd2c656
Shary Wang <145260199+sharyz119@users.noreply.github.com>
2024-05-27 23:50:18 +0200

    Update interactive.py

--- fairseq_easy_extend_cli/interactive.py
+++ fairseq_easy_extend_cli/interactive.py
@@ -126,11 +126,11 @@ def main(cfg: FairseqConfig):
         cfg.dataset.batch_size = 1
 
     assert (
-            not cfg.generation.sampling or cfg.generation.nbest == cfg.generation.beam
+        not cfg.generation.sampling or cfg.generation.nbest == cfg.generation.beam
     ), "--sampling requires --nbest to be equal to --beam"
     assert (
-            not cfg.dataset.batch_size
-            or cfg.dataset.batch_size <= cfg.interactive.buffer_size
+        not cfg.dataset.batch_size
+        or cfg.dataset.batch_size <= cfg.interactive.buffer_size
     ), "--batch-size cannot be larger than --buffer-size"
 
     logger.info(cfg)
@@ -210,101 +210,102 @@ def main(cfg: FairseqConfig):
     logger.info("NOTE: hypothesis and token scores are output in base 2")
     logger.info("Type the input sentence and press return:")
     start_id = 0
-    for inputs in buffered_read(cfg.interactive.input, cfg.interactive.buffer_size):
-        results = []
-        for batch in make_batches(inputs, cfg, task, max_positions, encode_fn):
-            bsz = batch.src_tokens.size(0)
-            src_tokens = batch.src_tokens
-            src_lengths = batch.src_lengths
-            constraints = batch.constraints
-            if use_cuda:
-                src_tokens = src_tokens.cuda()
-                src_lengths = src_lengths.cuda()
-                if constraints is not None:
-                    constraints = constraints.cuda()
+    with open(cfg.interactive.input, "r") as input_file, open(cfg.output_file, "w") as output_file:
+        for inputs in buffered_read(input_file, cfg.interactive.buffer_size):
+            results = []
+            for batch in make_batches(inputs, cfg, task, max_positions, encode_fn):
+                bsz = batch.src_tokens.size(0)
+                src_tokens = batch.src_tokens
+                src_lengths = batch.src_lengths
+                constraints = batch.constraints
+                if use_cuda:
+                    src_tokens = src_tokens.cuda()
+                    src_lengths = src_lengths.cuda()
+                    if constraints is not None:
+                        constraints = constraints.cuda()
 
-            sample = {
-                "net_input": {
-                    "src_tokens": src_tokens,
-                    "src_lengths": src_lengths,
-                },
-            }
-            translate_start_time = time.time()
-            translations = task.inference_step(
-                generator, models, sample, constraints=constraints
-            )
-            translate_time = time.time() - translate_start_time
-            total_translate_time += translate_time
-            list_constraints = [[] for _ in range(bsz)]
-            if cfg.generation.constraints:
-                list_constraints = [unpack_constraints(c) for c in constraints]
-            for i, (id, hypos) in enumerate(zip(batch.ids.tolist(), translations)):
-                src_tokens_i = utils.strip_pad(src_tokens[i], tgt_dict.pad())
-                constraints = list_constraints[i]
-                results.append(
-                    (
-                        start_id + id,
-                        src_tokens_i,
-                        hypos,
-                        {
-                            "constraints": constraints,
-                            "time": translate_time / len(translations),
-                        },
-                    )
+                sample = {
+                    "net_input": {
+                        "src_tokens": src_tokens,
+                        "src_lengths": src_lengths,
+                    },
+                }
+                translate_start_time = time.time()
+                translations = task.inference_step(
+                    generator, models, sample, constraints=constraints
                 )
-
-        # sort output to match input order
-        for id_, src_tokens, hypos, info in sorted(results, key=lambda x: x[0]):
-            src_str = ""
-            if src_dict is not None:
-                src_str = src_dict.string(src_tokens, cfg.common_eval.post_process)
-                print("S-{}\t{}".format(id_, src_str))
-                print("W-{}\t{:.3f}\tseconds".format(id_, info["time"]))
-                for constraint in info["constraints"]:
-                    print(
-                        "C-{}\t{}".format(
-                            id_,
-                            tgt_dict.string(constraint, cfg.common_eval.post_process),
+                translate_time = time.time() - translate_start_time
+                total_translate_time += translate_time
+                list_constraints = [[] for _ in range(bsz)]
+                if cfg.generation.constraints:
+                    list_constraints = [unpack_constraints(c) for c in constraints]
+                for i, (id, hypos) in enumerate(zip(batch.ids.tolist(), translations)):
+                    src_tokens_i = utils.strip_pad(src_tokens[i], tgt_dict.pad())
+                    constraints = list_constraints[i]
+                    results.append(
+                        (
+                            start_id + id,
+                            src_tokens_i,
+                            hypos,
+                            {
+                                "constraints": constraints,
+                                "time": translate_time / len(translations),
+                            },
                         )
                     )
 
-            # Process top predictions
-            for hypo in hypos[: min(len(hypos), cfg.generation.nbest)]:
-                hypo_tokens, hypo_str, alignment = utils.post_process_prediction(
-                    hypo_tokens=hypo["tokens"].int().cpu(),
-                    src_str=src_str,
-                    alignment=hypo["alignment"],
-                    align_dict=align_dict,
-                    tgt_dict=tgt_dict,
-                    remove_bpe=cfg.common_eval.post_process,
-                    extra_symbols_to_ignore=get_symbols_to_strip_from_output(generator),
-                )
-                detok_hypo_str = decode_fn(hypo_str)
-                score = hypo["score"]  # convert to base 2
-                # original hypothesis (after tokenization and BPE)
-                print("H-{}\t{}\t{}".format(id_, score, hypo_str))
-                # detokenized hypothesis
-                print("D-{}\t{}\t{}".format(id_, score, detok_hypo_str))
-                print(
-                    "P-{}\t{}".format(
-                        id_,
-                        " ".join(
-                            map(
-                                lambda x: "{:.4f}".format(x),
-                                # convert from base e to base 2
-                                hypo["positional_scores"].tolist(),
+            # sort output to match input order
+            for id_, src_tokens, hypos, info in sorted(results, key=lambda x: x[0]):
+                src_str = ""
+                if src_dict is not None:
+                    src_str = src_dict.string(src_tokens, cfg.common_eval.post_process)
+                    output_file.write("S-{}\t{}\n".format(id_, src_str))
+                    output_file.write("W-{}\t{:.3f}\tseconds\n".format(id_, info["time"]))
+                    for constraint in info["constraints"]:
+                        output_file.write(
+                            "C-{}\t{}\n".format(
+                                id_,
+                                tgt_dict.string(constraint, cfg.common_eval.post_process),
                             )
-                        ),
-                    )
-                )
-                if cfg.generation.print_alignment:
-                    alignment_str = " ".join(
-                        ["{}-{}".format(src, tgt) for src, tgt in alignment]
-                    )
-                    print("A-{}\t{}".format(id_, alignment_str))
+                        )
 
-        # update running id_ counter
-        start_id += len(inputs)
+                # Process top predictions
+                for hypo in hypos[: min(len(hypos), cfg.generation.nbest)]:
+                    hypo_tokens, hypo_str, alignment = utils.post_process_prediction(
+                        hypo_tokens=hypo["tokens"].int().cpu(),
+                        src_str=src_str,
+                        alignment=hypo["alignment"],
+                        align_dict=align_dict,
+                        tgt_dict=tgt_dict,
+                        remove_bpe=cfg.common_eval.post_process,
+                        extra_symbols_to_ignore=get_symbols_to_strip_from_output(generator),
+                    )
+                    detok_hypo_str = decode_fn(hypo_str)
+                    score = hypo["score"]  # convert to base 2
+                    # original hypothesis (after tokenization and BPE)
+                    output_file.write("H-{}\t{}\t{}\n".format(id_, score, hypo_str))
+                    # detokenized hypothesis
+                    output_file.write("D-{}\t{}\t{}\n".format(id_, score, detok_hypo_str))
+                    output_file.write(
+                        "P-{}\t{}\n".format(
+                            id_,
+                            " ".join(
+                                map(
+                                    lambda x: "{:.4f}".format(x),
+                                    # convert from base e to base 2
+                                    hypo["positional_scores"].tolist(),
+                                )
+                            ),
+                        )
+                    )
+                    if cfg.generation.print_alignment:
+                        alignment_str = " ".join(
+                            ["{}-{}".format(src, tgt) for src, tgt in alignment]
+                        )
+                        output_file.write("A-{}\t{}\n".format(id_, alignment_str))
+
+            # update running id_ counter
+            start_id += len(inputs)
 
     logger.info(
         "Total time: {:.3f} seconds; translation time: {:.3f}".format(
@@ -315,6 +316,12 @@ def main(cfg: FairseqConfig):
 
 def cli_main():
     parser = options.get_interactive_generation_parser()
+    parser.add_argument(
+        "--iter-decode-with-beam",
+        type=int,
+        default=None,
+        help="perform beam search during iterative decoding with beam size N",
+    )
     args = options.parse_args_and_arch(parser)
     distributed_utils.call_main(convert_namespace_to_omegaconf(args), main)
 
